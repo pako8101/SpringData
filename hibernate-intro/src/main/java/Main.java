d82b66625bffc299ce2662dada43e004dd95e729
Plamen Kamenov <pako810129@gmail.com>
2023-08-22 18:48:09 +0300

    Add files via upload

---
+++ hibernate-intro/src/main/java/Main.java
@@ -0,0 +1,31 @@
+import entities.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Configuration configuration = new Configuration();
+        configuration.configure();
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        Student example = new Student();
+        example.setName("Bobo");
+        session.save(example);
+
+        List<Student>students= session.createQuery("FROM Student as s where s.name = 'Tosho'", Student.class).list();
+        students.forEach(student -> System.out.println(student.getId()+ student.getName()));
+
+        session.get(Student.class,1);
+        session.getTransaction().commit();
+        session.close();
+    }
+}
